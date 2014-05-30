c0f41507992f52b2e1a948054bd38e283c9b69a9
ScreenDriver <christian@echooff.de>
2014-05-30 15:57:36 +0200

    Added arguments to chuck_norris()

--- myjabberbot.py
+++ myjabberbot.py
@@ -13,14 +13,22 @@ class MyJabberBot(JabberBot):
 
     @botcmd
     def chuck_norris(self, mess, args):
-        """Displays a random Chuck Norris joke from http://icndb.com"""
-        data = urllib2.urlopen('http://api.icndb.com/jokes/random')
-        parsedJson = json.load(data)
-        parser = htmllib.HTMLParser(None)
-        parser.save_bgn()
-        parser.feed(parsedJson['value']['joke'])
-        return parser.save_end()
+        """Displays a random Chuck Norris joke from http://icndb.com
 
+You can change the name of the main character by appending it as arguments: chuck_norris firstnam lastname
+        """
+        url = 'http://api.icndb.com/jokes/random'
+        if args:
+            names = args.split(' ')
+            if len(names) != 2:
+                return 'You must append one firstname and one lastname'
+            url += '?firstName=%s&lastName=%s' % (names[0], names[1])
+            data = urllib2.urlopen(url)
+            parsedJson = json.load(data)
+            parser = htmllib.HTMLParser(None) # Escape HTML
+            parser.save_bgn()
+            parser.feed(parsedJson['value']['joke'])
+            return parser.save_end()
 
 if __name__ == '__main__':
     username = ''
