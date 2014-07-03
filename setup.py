5255c1b0743dc44b0fcc9b0ea0f801202fa4ac69
ScreenDriver <christian@echooff.de>
2014-07-03 16:29:52 +0200

    Removed Python 3.3 from Travis build

--- setup.py
+++ setup.py
@@ -9,6 +9,12 @@ setup(
     author_email='christian@echooff.de',
     license='MIT',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
     install_requires=[
         'dnspython3>=1.11.1',
         'feedparser>=5.1.3',
