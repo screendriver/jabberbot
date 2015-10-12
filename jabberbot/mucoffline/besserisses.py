00ae27ecd6370b8ad5bda1cd7fe876d4d730f1f5
Christian Rackerseder <git@echooff.de>
2015-10-12 14:32:17 +0200

    Added timer to besserisses
    Fixes #19

--- jabberbot/mucoffline/besserisses.py
+++ jabberbot/mucoffline/besserisses.py
@@ -1,7 +1,11 @@
-import random
+from datetime import datetime, timedelta
+
+min_delta = timedelta(hours=1)
+start = datetime.now()
 
 
 def muc_got_offline(nick):
-    if random.choice([True, False]):
+    if (datetime.now() - start) > min_delta:
+        global start
+        start = datetime.now()
         return '{} hat den Raum verlassen? Besser isses'.format(nick)
-    return None
--- jabberbot/test/mucoffline/test_besserisses.py
+++ jabberbot/test/mucoffline/test_besserisses.py
@@ -1,16 +1,23 @@
-import random
+from datetime import datetime, timedelta
 import unittest
 from unittest.mock import patch
 from jabberbot.mucoffline import besserisses
 
 
-@patch.object(random, 'choice')
+@patch.object(besserisses, 'datetime')
 class TestBesserIsses(unittest.TestCase):
-    def test_muc_got_offline(self, mock_random):
-        mock_random.return_value = True
-        message = besserisses.muc_got_offline('foo')
-        self.assertEqual(message, 'foo hat den Raum verlassen? Besser isses')
+    def test_timedelta(self, mock_datetime):
+        self.assertEqual(besserisses.min_delta, timedelta(hours=1))
 
-        mock_random.return_value = False
+    def test_muc_got_offline_after_30_minutes(self, mock_datetime):
+        now = datetime.now() + timedelta(minutes=30)
+        mock_datetime.now.side_effect = lambda: now
         message = besserisses.muc_got_offline('foo')
         self.assertIsNone(message)
+
+    def test_muc_got_offline_after_65_minutes(self, mock_datetime):
+        now = datetime.now() + timedelta(minutes=65)
+        mock_datetime.now.side_effect = lambda: now
+        message = besserisses.muc_got_offline('foo')
+        self.assertEqual(message, 'foo hat den Raum verlassen? Besser isses')
+        self.assertEqual(besserisses.start, now)
