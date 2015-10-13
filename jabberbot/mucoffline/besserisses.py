e86c87b0b873499c2392e017865311846365d545
Christian Rackerseder <git@echooff.de>
2015-10-13 10:09:23 +0200

    New MUC leave messages

--- jabberbot/mucoffline/besserisses.py
+++ jabberbot/mucoffline/besserisses.py
@@ -1,11 +1,19 @@
 from datetime import datetime, timedelta
+import random
 
 min_delta = timedelta(hours=1)
 start = datetime.now()
+messages = [
+    '{} hat den Raum verlassen? Besser isses!',
+    'Zum Glück ist {} freiwillig gegangen',
+    'Endlich hat {} den Raum verlassen',
+    'Pfiat di {}',
+    '{} ist gerade noch so einem Kick entkommen',
+]
 
 
 def muc_got_offline(nick):
+    global start
     if (datetime.now() - start) > min_delta:
-        global start
         start = datetime.now()
-        return '{} hat den Raum verlassen? Besser isses'.format(nick)
+        return random.choice(messages).format(nick)
--- jabberbot/test/mucoffline/test_besserisses.py
+++ jabberbot/test/mucoffline/test_besserisses.py
@@ -1,23 +1,43 @@
 from datetime import datetime, timedelta
+import random
 import unittest
 from unittest.mock import patch
 from jabberbot.mucoffline import besserisses
 
 
+@patch.object(random, 'choice')
 @patch.object(besserisses, 'datetime')
 class TestBesserIsses(unittest.TestCase):
-    def test_timedelta(self, mock_datetime):
+    def test_timedelta(self, mock_datetime, mock_choice):
         self.assertEqual(besserisses.min_delta, timedelta(hours=1))
 
-    def test_muc_got_offline_after_30_minutes(self, mock_datetime):
+    def test_muc_got_offline_after_30_minutes(self, mock_datetime,
+                                              mock_choice):
         now = datetime.now() + timedelta(minutes=30)
         mock_datetime.now.side_effect = lambda: now
         message = besserisses.muc_got_offline('foo')
         self.assertIsNone(message)
 
-    def test_muc_got_offline_after_65_minutes(self, mock_datetime):
+    def test_muc_got_offline_after_65_minutes(self, mock_datetime,
+                                              mock_choice):
+        old_start = besserisses.start
         now = datetime.now() + timedelta(minutes=65)
-        mock_datetime.now.side_effect = lambda: now
+        mock_datetime.now.return_value = now
+        messages = iter(besserisses.messages)
+        mock_choice.side_effect = lambda msgs: next(messages)
         message = besserisses.muc_got_offline('foo')
-        self.assertEqual(message, 'foo hat den Raum verlassen? Besser isses')
+        self.assertEqual(message, 'foo hat den Raum verlassen? Besser isses!')
+        besserisses.start = old_start
+        message = besserisses.muc_got_offline('foo')
+        self.assertEqual(message, 'Zum Glück ist foo freiwillig gegangen')
+        besserisses.start = old_start
+        message = besserisses.muc_got_offline('foo')
+        self.assertEqual(message, 'Endlich hat foo den Raum verlassen')
+        besserisses.start = old_start
+        message = besserisses.muc_got_offline('foo')
+        self.assertEqual(message, 'Pfiat di foo')
+        besserisses.start = old_start
+        message = besserisses.muc_got_offline('foo')
+        self.assertEqual(message,
+                         'foo ist gerade noch so einem Kick entkommen')
         self.assertEqual(besserisses.start, now)
